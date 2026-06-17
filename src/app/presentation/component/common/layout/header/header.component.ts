88a01e96753c56f33081ff1d022ee43a37d1a4f1
steveencues <zh311505@gmail.com>
2026-06-16 23:09:21 -0500

    fix(header): mejorar el header

--- src/app/presentation/component/common/layout/header/header.component.ts
+++ src/app/presentation/component/common/layout/header/header.component.ts
@@ -11,6 +11,7 @@ import { AvatarModule } from 'primeng/avatar';
 import { MegaMenuModule } from 'primeng/megamenu';
 import { RippleModule } from 'primeng/ripple';
 import type { MegaMenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 /**
  * @component HeaderComponent
